cedf61b94e62ee904e34af3a6cebcfaed23a7686
PhilHou <1419410411@qq.com>
2021-05-06 13:58:34 +0800

    用户登录-创建页面组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 在vue/cli创建的項目中 @表示 src 目录
+// 它是src目录的路径别名 好处就是不受当前路径影响
+// 注意：后面还需要加/   建议：如果需要父级路径查找的都使用@
+import Login from '@/views/login/index.vue'
+
+
 Vue.use(VueRouter)
 
 // 路由配置表
-const routes = []
+const routes = [{
+    path: '/login',
+    name: 'login',
+    component: Login
+}]
 
 const router = new VueRouter({
     routes
---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="login-container">
+    登录页面
+</div>
+</template>
+
+<script>
+    export default {
+        // 数据存放区域
+        name: 'LoginIndex',
+        component: {},
+        data() {
+            return {}
+        },
+        // 生命周期函数
+        created() {},
+        // 方法函数
+        methods: {},
+        // 计算属性
+        computed: {}
+    }
+</script>
+
+<style lang='less' scoped>
+
+</style>
