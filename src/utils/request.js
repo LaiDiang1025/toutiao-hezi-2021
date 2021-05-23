9c51b72036648de05d9825d088123e9f0ce232f6
PhilHou <1419410411@qq.com>
2021-05-23 10:56:56 +0800

    github提交

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,9 @@
 
 import axios from 'axios'
 import JSONbig from 'json-bigint'
+import router from '@/router'
+// 非组件模块可以这样加载使用 message 提示组件
+import {Message} from 'element-ui'
 
 // 创建一个 axios 实例，说白了就是复制了一个 axios 和axios本身的功能一模一样
 // 我们通过这个实例去发请求，把需要的配置配置给这个实例来处理
@@ -52,7 +55,37 @@ request.interceptors.request.use(
     }
 )
 
-// 响应拦截器
+// 添加响应拦截器
+request.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    // response 是响应数据
+    // 必须return response
+    return response;
+  }, function (error) {
+      const {status} = error.response
+    // 对响应错误做点什么
+    if(error.response && status  === 401){
+        //跳转到登录页面
+        // 清除本地存储中的用户登录状态
+        Message.error('登录状态无效，请重新登录')
+        window.localStorage.removeItem('user')
+        router.push('/login')
+
+    }else if(status === 403){
+        //token未携带或已过期
+        Message.error('token未携带或已过期')
+
+    }else if(status === 400){
+        // 客户端参数错误
+        Message.error('客户端参数错误')
+
+    }else if(status >= 500){
+        // 服务端内部错误
+        Message.error('服务端内部错误')
+    }
+    return Promise.reject(error);
+  })
+
 
 // 导出请求方法
 export default request
